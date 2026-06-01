a13543ab31ed3ed8389f9dc1455d2e6bd6bf1e26
Muntacir-Tamim <muntacirtamim202162@gmail.com>
2026-06-01 16:56:00 +0600

    implement issue controller and routes with filtering, sorting, and RBAC

--- src/api/routes/issue.routes.ts
+++ src/api/routes/issue.routes.ts
@@ -1,22 +1,22 @@
-// import { Router } from "express";
-// import { auth, authorizeRoles } from "../../middleware/auth";
-// import {
-//   createIssue,
-//   deleteIssue,
-//   getAllIssues,
-//   getIssueById,
-//   updateIssue,
-// } from "../controllers/issue.controller";
+import { Router } from "express";
+import { auth, authorizeRoles } from "../../middleware/auth";
+import {
+  createIssue,
+  deleteIssue,
+  getAllIssues,
+  getIssueById,
+  updateIssue,
+} from "../controllers/issue.controller";
 
-// const router = Router();
+const router = Router();
 
-// router.get("/", getAllIssues);
-// router.get("/:id", getIssueById);
+router.get("/", getAllIssues);
+router.get("/:id", getIssueById);
 
-// router.post("/", auth, createIssue);
+router.post("/", auth, createIssue);
 
-// router.patch("/:id", auth, updateIssue);
+router.patch("/:id", auth, updateIssue);
 
-// router.delete("/:id", auth, authorizeRoles("maintainer"), deleteIssue);
+router.delete("/:id", auth, authorizeRoles("maintainer"), deleteIssue);
 
-// export default router;
+export default router;
