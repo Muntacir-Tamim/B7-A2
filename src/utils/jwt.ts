cc568612e0fd7fcd7e42392b5dc767ee8c2fcc8e
Muntacir-Tamim <muntacirtamim202162@gmail.com>
2026-06-01 15:18:28 +0600

    add custom sendResponse utility and JWT helper functions

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,16 +1,16 @@
-// import jwt, { type JwtPayload } from "jsonwebtoken";
-// import { config } from "../config";
-// import type { RUser } from "../types";
+import jwt, { type JwtPayload } from "jsonwebtoken";
+import { config } from "../config";
+import type { RUser } from "../types";
 
-// export const signToken = (payload: RUser): string => {
-//   const { id, name, role } = payload;
-//   return jwt.sign({ id, name, role }, config.secret, { expiresIn: "7d" });
-// };
+export const signToken = (payload: RUser): string => {
+  const { id, name, role } = payload;
+  return jwt.sign({ id, name, role }, config.secret, { expiresIn: "7d" });
+};
 
-// export const verifyToken = (token: string): (JwtPayload & RUser) | null => {
-//   try {
-//     return jwt.verify(token, config.secret) as JwtPayload & RUser;
-//   } catch {
-//     return null;
-//   }
-// };
+export const verifyToken = (token: string): (JwtPayload & RUser) | null => {
+  try {
+    return jwt.verify(token, config.secret) as JwtPayload & RUser;
+  } catch {
+    return null;
+  }
+};
